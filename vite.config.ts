1ee8b170e89ebd920f7ac44b0a828080090defc3
Pedro Alves <pedro.alves@ifood.com.br>
2025-05-11 11:32:30 -0300

    teste

--- vite.config.ts
+++ vite.config.ts
@@ -5,18 +5,26 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+
   server: {
     host: "::",
     port: 8080,
   },
+  
   plugins: [
     react(),
     mode === 'development' &&
     componentTagger(),
   ].filter(Boolean),
+  
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
+  },
+
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
   },
 }));
